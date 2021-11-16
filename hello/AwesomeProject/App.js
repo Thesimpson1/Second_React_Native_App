82e21201ee6151ccab7dd3a429acdb7d1ba73256
IvanHassay <ivanrndev@gmail.com>
2021-11-16 11:36:25 +0200

    init

---
+++ hello/AwesomeProject/App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
